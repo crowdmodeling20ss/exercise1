d96ca44175c64a7dfb89b7ba3b2488acbb808ecf
aynursune <aynur.suene@tum.de>
2020-04-26 21:58:35 +0200

    Reading from a .txt file

--- Constant.py
+++ Constant.py
@@ -3,6 +3,19 @@ S_PEDESTRIAN = 1
 S_OBSTACLE = 2
 S_TARGET = 4
 
+# Default values
+S_FILENAME = "grid_setup.txt"
+S_SPEED = 0
+S_AVOIDANCE = 0
+
+# Array and tuple name formats in grid_setup.txt
+S_GRID_SIZE_TUPLE_NAME = "Grid Size"
+S_PEDESTRIAN_LOCATION_ARR_NAME = "Pedestrian"
+S_TARGET_LOCATION_ARR_NAME = "Target"
+S_OBSTACLE_LOCATION_ARR_NAME = "Obstacle"
+S_SPEED_INT_NAME = "Speed"
+S_AVOIDANCE_INT_NAME = "Avoidance"
+
 # TODO: We can increase number of direction later
 DIRECTIONS = [[-1, 0],  # North
               [0, -1],  # East
---
+++ FileRead.py
@@ -0,0 +1,50 @@
+import numpy as np
+import ast
+from Constant import *
+
+def read_setup_file():
+    pedestrian_locations = []
+    target_locations = []
+    obstacle_locations = []
+    grid_size = ()
+    speed = S_SPEED
+    avoidance = S_AVOIDANCE
+
+    f = open(S_FILENAME, "r")
+    lines = f.readlines()
+
+    for line in lines:
+        setup = line.strip().split("=")
+        setup[0] = setup[0].strip().lower()
+        setup[1] = setup[1].strip()
+        obj = ast.literal_eval(setup[1])
+        print(type(obj))
+        if setup[0] == S_GRID_SIZE_TUPLE_NAME.lower():
+            grid_size = obj
+        elif setup[0] == S_PEDESTRIAN_LOCATION_ARR_NAME.lower():
+            pedestrian_locations = obj
+        elif setup[0] == S_TARGET_LOCATION_ARR_NAME.lower():
+            target_locations = obj
+        elif setup[0] == S_OBSTACLE_LOCATION_ARR_NAME.lower():
+            obstacle_locations = obj
+        elif setup[0] == S_SPEED_INT_NAME.lower():
+            speed = obj
+        elif setup[0] == S_AVOIDANCE_INT_NAME.lower():
+            avoidance = obj
+
+    return grid_size, pedestrian_locations, target_locations, obstacle_locations, speed, avoidance
+
+
+def create_grid():
+    grid_size, pedestrian_locations, target_locations, obstacle_locations, speed, avoidance = read_setup_file()
+    grid = np.zeros(grid_size)
+
+    for loc in pedestrian_locations:
+        grid[loc] = S_PEDESTRIAN
+    for loc in target_locations:
+        grid[loc] = S_TARGET
+    for loc in obstacle_locations:
+        grid[loc] = S_OBSTACLE
+
+    return grid
+
