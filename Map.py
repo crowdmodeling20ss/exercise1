7148d409ffccdce84c7a042b4950298c31517b75
Faruk Cankaya <farukcankayaaa@gmail.com>
2020-05-03 09:20:20 +0300

    add dijkstra enabled/disabled feature to test chicken test

--- Map.py
+++ Map.py
@@ -1,4 +1,7 @@
 import concurrent.futures
+import math
+
+import numpy as np
 
 from Constant import *
 from Dijkstra import Dijkstra
@@ -14,13 +17,14 @@ class Map:
     :param data MXN numpy array
     """
 
-    def __init__(self, width, height, data, corners):
+    def __init__(self, width, height, data, corners, is_dijkstra_enabled=True):
         self.id = 0
         self.width = width
         self.height = height
         self.data = data
-        self.cost_map = self.calculate_cost_map()
         self.corners = corners
+        self.is_dijkstra_enabled = is_dijkstra_enabled
+        self.cost_map = self.calculate_cost_map()
 
     def set_state(self, position, state):
         self.data[position[0]][position[1]] = state
@@ -143,22 +147,102 @@ class Map:
 
     ##TODO: MULTICELL GET NEIGHBOR WITH COST
     def get_neighbours_multicell(self, corners, size):
-
         direction_cost = [0, 0, 0, 0]  # costs for top, right, bottom, left direction
 
-        for i in range(4):
-            # print("i", i)
-            with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-                # future = executor.submit(foo, 'world!')
-                future = executor.submit(self.thread_function, corners, size, i)
-                direction_cost[i] = future.result()
-        # direction_cost[D_TOP] = self.neighbour_check_top(corners, size)
-        # direction_cost[D_RIGHT] = self.neighbour_check_right(corners, size)
-        # direction_cost[D_BOTTOM] = self.neighbour_check_bottom(corners, size)
-        # direction_cost[D_LEFT] = self.neighbour_check_left(corners, size)
+        if self.is_dijkstra_enabled:
+            for i in range(4):
+                # print("i", i)
+                with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+                    # future = executor.submit(foo, 'world!')
+                    future = executor.submit(self.thread_function, corners, size, i)
+                    direction_cost[i] = future.result()
+            # direction_cost[D_TOP] = self.neighbour_check_top(corners, size)
+            # direction_cost[D_RIGHT] = self.neighbour_check_right(corners, size)
+            # direction_cost[D_BOTTOM] = self.neighbour_check_bottom(corners, size)
+            # direction_cost[D_LEFT] = self.neighbour_check_left(corners, size)
 
-        # print("The direction costs: ", direction_cost)
-        return direction_cost
+            # print("The direction costs: ", direction_cost)
+            return direction_cost
+        else:
+            for direction in range(4):
+                direction_cost[direction] = self.ddd(direction, corners, size)
+
+            return direction_cost
+
+    def ddd(self, direction, corners, size):
+        direction_cost = 0
+        if direction == D_TOP:
+            if 0 <= (corners[0][0] - 1) < len(self.data):
+                for i in range(size):
+                    curr_point = self.data[corners[0][0] - 1][corners[0][1] + i]
+                    if curr_point == S_TARGET:
+                        direction_cost = -1
+                        return direction_cost
+                        ##EXIT FUNCTION, found the target
+                    elif curr_point == S_OBSTACLE or curr_point == S_PEDESTRIAN:
+                        direction_cost = -2  # invalid neighbour
+                        return direction_cost
+                        # Check if break works:
+                    else:
+                        direction_cost += self.calculate_distance_cost([corners[0][0] - 1, corners[0][1] + i])
+                return direction_cost / 4.0
+            else:
+                direction_cost = -2  # invalid neighbour
+                return direction_cost
+        elif direction == D_RIGHT:
+            if 0 <= (corners[1][1] + 1) < len(self.data[0]):
+                for i in range(size):
+                    curr_point = self.data[corners[1][0] + i][corners[1][1] + 1]
+                    if curr_point == S_TARGET:
+                        direction_cost = -1
+                        return direction_cost
+                        ##EXIT FUNCTION, found the target
+                    elif curr_point == S_OBSTACLE or curr_point == S_PEDESTRIAN:
+                        direction_cost = -2  # invalid neighbour
+                        return direction_cost
+
+                    else:
+                        direction_cost += self.calculate_distance_cost([corners[1][0] + i, corners[1][1] + 1])
+                return direction_cost / 4.0
+            else:
+                direction_cost = -2  # invalid neighbour
+                return direction_cost
+        elif direction == D_BOTTOM:
+            if 0 <= (corners[2][0] + 1) < len(self.data):
+                for i in range(size):
+                    curr_point = self.data[corners[2][0] + 1][corners[2][1] + i]
+                    if curr_point == S_TARGET:
+                        direction_cost = -1
+                        return direction_cost
+                        ##EXIT FUNCTION, found the target
+                    elif curr_point == S_OBSTACLE or curr_point == S_PEDESTRIAN:
+                        direction_cost = -2  # invalid neighbour
+                        # Check if break works:
+                        return direction_cost
+                    else:
+                        direction_cost += self.calculate_distance_cost([corners[2][0] + 1, corners[2][1] + i])
+                return direction_cost / 4.0
+            else:
+                direction_cost = -2  # invalid neighbour
+                return direction_cost
+        elif direction == D_LEFT:
+            if 0 <= (corners[0][1] - 1) < len(self.data[0]):
+                for i in range(size):
+                    curr_point = self.data[corners[0][0] + i][corners[0][1] - 1]
+                    if curr_point == S_TARGET:
+                        direction_cost = -1
+                        return direction_cost
+                        ##EXIT FUNCTION, found the target
+                    elif curr_point == S_OBSTACLE or curr_point == S_PEDESTRIAN:
+                        direction_cost = -2  # invalid neighbour
+                        # Check if break works:
+                        return direction_cost
+                    else:
+                        direction_cost += self.calculate_distance_cost([corners[0][0] + i, corners[0][1] - 1])
+                return direction_cost / 4.0
+            else:
+                direction_cost = -2  # invalid neighbour
+                return direction_cost
 
     def set_state_multicell(self, pos, direction, size, block_type):  # to change a row or column
         cell_x = pos[0]
@@ -194,10 +278,31 @@ class Map:
     Calculate cost starting from target and save costs in cost_map 
     """
 
-    def calculate_cost_map(self, djiksta_boolean=1):
-        if (djiksta_boolean == 1):
+    def calculate_cost_map(self):
+        if self.is_dijkstra_enabled:
             return Dijkstra(self, self.data).calculate_cost_map()
         return 0
 
     def get_cost(self, position):
         return self.cost_map[position[0]][position[1]]
+
+    def calculate_distance_cost(self, neighbour_position):
+        min_distance = math.inf
+        ns = self.get_target_positions()
+        nearest_row=math.inf
+        nearest_column=math.inf
+        for t in ns:
+            if abs(t[0]-neighbour_position[0]) < nearest_row:
+                nearest_row = t[0]
+            if abs(t[1]-neighbour_position[1]) < nearest_column:
+                nearest_column = t[1]
+        min_distance = np.linalg.norm(np.array([nearest_row, nearest_column]) - np.array(neighbour_position))
+        # normalize
+        min_distance = (min_distance*1.0) / np.linalg.norm(np.array([0,0]) - np.array([0, len(self.data[0])-1]))
+        ''' DO NOT CALCULATE NORM EACH TIME
+        for t in self.grid_map.get_target_positions():
+            distance = np.linalg.norm(np.array(t) - np.array(neighbour_position))
+            if distance < min_distance:
+                min_distance = distance
+        '''
+        return min_distance
