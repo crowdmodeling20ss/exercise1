c31de1e38dbac05548113301e427ac63d0a6ac8d
Faruk Cankaya <farukcankayaaa@gmail.com>
2020-05-05 00:30:54 +0300

    user defined simulations

--- ConfigurableSimulation.py
+++ ConfigurableSimulation.py
@@ -1,20 +1,18 @@
 from CellularModel import CellularModel
 from GridCreator import GridCreator
 from Map import Map
-from ScenarioGrids import create_grid
+from NewScenarios import create_grid_new_scenario
 from Simulation import Simulation
 
 
-class PredefinedSimulation(Simulation):
-
+class ConfigurableSimulation(Simulation):
     def run(self):
         self._is_running = True
-        CONFIGURABLE_grid, is_dijkstra_enabled, is_pedestrian_exit = create_grid()
+
+        CONFIGURABLE_grid, is_dijkstra_enabled = create_grid_new_scenario()
         print("is dijkstra enabled ", is_dijkstra_enabled)
-        print("is_pedestrian_exit ", is_pedestrian_exit)
         self.IS_DIJKSTRA_ENABLED = is_dijkstra_enabled
         self.SHOW_COST_MAP = is_dijkstra_enabled
-        self.IS_PEDESTRIAN_EXIT = is_pedestrian_exit
 
         CONFIGURABLE_the_grid = GridCreator(CONFIGURABLE_grid, 1)
         CONFIGURABLE_map_obj = Map(CONFIGURABLE_the_grid.grid.shape[0], CONFIGURABLE_the_grid.grid.shape[1],
@@ -26,4 +24,3 @@ class PredefinedSimulation(Simulation):
         CONFIGURABLE_model = CellularModel(CONFIGURABLE_map_obj, self.IS_PEDESTRIAN_EXIT, self.SPEED_OF_PEDESTRIANS,
                                            self.SPEED_PER_PEDESTRIAN_IS_ON)
         self.runSimulation(CONFIGURABLE_model, self.SHOW_SPEED_GRAPH)
-        # """
